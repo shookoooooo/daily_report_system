6acf447b77eb617ae5e9295cd25a8ecbdb9985eb
S <sho.ikma.0116@gmail.com>
2021-09-21 21:34:53 +0900

    従業員indexアクションの作成

--- src/main/java/controllers/FrontController.java
+++ src/main/java/controllers/FrontController.java
@@ -45,7 +45,7 @@ public class FrontController extends HttpServlet {
 
             String actionString = request.getParameter(ForwardConst.ACT.getValue());
 
-            type = Class.forName(String.format("action.%Action", actionString));
+            type = Class.forName(String.format("actions.%sAction", actionString));
 
             action = (ActionBase)(type.asSubclass(ActionBase.class)
                     .getDeclaredConstructor()
